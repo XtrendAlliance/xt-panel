3ec0f1bb5e516d4dca46bd933e128c69f4a8170d
max <maximo@astrofonix.com>
2013-04-22 12:36:20 +0100

    fixed user script

--- usr/lib/enigma2/python/Plugins/SystemPlugins/XTPanel/Scripts.py
+++ usr/lib/enigma2/python/Plugins/SystemPlugins/XTPanel/Scripts.py
@@ -1,76 +1,82 @@
+# for localized messages
+from . import _
+from Plugins.Plugin import PluginDescriptor
 from Screens.Screen import Screen
-from Screens.MessageBox import MessageBox
 from Screens.Console import Console
-from Components.ActionMap import ActionMap
+from Screens.ChoiceBox import ChoiceBox
+from Screens.MessageBox import MessageBox
+from Components.ActionMap import ActionMap, HelpableActionMap
 from Components.Label import Label
+from Components.Language import language
 from Components.Button import Button
-from Components.ScrollLabel import ScrollLabel
 from Components.MenuList import MenuList
-from Extra.BoxInfo import BoxInfo
-from os import system, listdir
-from __init__ import _, loadPluginSkin
+from Components.Sources.List import List
+from Screens.Standby import TryQuitMainloop
+from Tools.Directories import resolveFilename, SCOPE_PLUGINS, SCOPE_CURRENT_SKIN
+from os import listdir, remove, mkdir, path, access, X_OK, chmod
+import datetime, time
 
 class Scripts(Screen):
+	skin = """<screen name="ScriptRunner" position="center,center" size="560,400" title="Script Runner" flags="wfBorder" >
+		<ePixmap pixmap="skin_default/buttons/red.png" position="0,0" size="140,40" alphatest="on" />
+		<ePixmap pixmap="skin_default/buttons/green.png" position="140,0" size="140,40" alphatest="on" />
+		<widget name="key_red" position="0,0" zPosition="1" size="140,40" font="Regular;20" halign="center" valign="center" backgroundColor="#9f1313" transparent="1" />
+		<widget name="key_green" position="140,0" zPosition="1" size="140,40" font="Regular;20" halign="center" valign="center" backgroundColor="#1f771f" transparent="1" />
+		<widget name="lab1" position="0,50" size="560,50" font="Regular; 20" zPosition="2" transparent="0" halign="center"/>
+		<widget name="list" position="10,105" size="540,300" scrollbarMode="showOnDemand" />
+		<applet type="onLayoutFinish">
+			self["list"].instance.setItemHeight(25)
+		</applet>
+	</screen>"""
 
-    def __init__(self, session):
-        Screen.__init__(self, session)
-        boxinfo = BoxInfo()
-        boxinfo.detectBox()
-        self.scriptsPath = boxinfo.scriptsPath
-        self['statuslab'] = Label('')
-        self['key_red'] = Button(_('Execute'))
-        self['key_green'] = Button('')
-        self['key_yellow'] = Button('')
-        self['key_blue'] = Button(_('Exit'))
-        self.mlist = []
-        try:
-            self.populateSL()
-        except OSError:
-            print '[CTPanel] Could not find any /usr/script Folder to populate'
 
-        self['list'] = MenuList(self.mlist)
-        self['list'].onSelectionChanged.append(self.schanged)
-        self['actions'] = ActionMap(['WizardActions', 'ColorActions'], {'ok': self.mygo,
-         'back': self.close,
-         'blue': self.close,
-         'red': self.mygo})
-        self.onLayoutFinish.append(self.refr_sel)
+	def __init__(self, session):
+		Screen.__init__(self, session)
+		Screen.setTitle(self, _("Job Manager"))
+		self['lab1'] = Label()
+		self.list = []
+		self.populate_List()
+		self['list'] = MenuList(self.list)
+		self['myactions'] = ActionMap(['ColorActions', 'OkCancelActions', 'DirectionActions'],
+			{
+				'cancel': self.close,
+				'red': self.close,
+				'green': self.runscript,
+				'ok': self.runscript,
+			}, -1)
 
-    def refr_sel(self):
-        self['list'].moveToIndex(1)
-        self['list'].moveToIndex(0)
+		self["key_red"] = Button(_("Close"))
+		self["key_green"] = Button(_("Run"))
+		
+	def populate_List(self):
+		if not path.exists('/usr/scripts'):
+			mkdir('/usr/scripts', 0755)
+		self['lab1'].setText(_("Select a script to run:"))
+		del self.list[:]
+		f = listdir('/usr/scripts')
+		for line in f:
+			parts = line.split()
+			pkg = parts[0]
+			if pkg.find('.sh') >= 0:
+				self.list.append(pkg)
+		self.list.sort()	
 
-    def populateSL(self):
-        self.scriptdesc = []
-        try:
-            myscripts = listdir(self.scriptsPath)
-            print myscripts
-            for fil in myscripts:
-                if fil.find('.sh') != -1:
-                    fil2 = fil[:-3]
-                    desc = 'N/A'
-                    print self.scriptsPath + fil
-                    f = open(self.scriptsPath + fil, 'r')
-                    for line in f.readlines():
-                        if line.find('#DESCRIPTION=') != -1:
-                            line = line.strip()
-                            desc = line[13:]
+	def runscript(self):
+		self.sel = self['list'].getCurrent()
+		if self.sel:
+			message = _("Are you ready to run the script ?")
+			ybox = self.session.openWithCallback(self.Run, MessageBox, message, MessageBox.TYPE_YESNO)
+			ybox.setTitle(_("Run Confirmation"))
+		else:
+			self.session.open(MessageBox, _("You have no script to run."), MessageBox.TYPE_INFO, timeout = 10)
 
-                    f.close()
-                    self.mlist.append(fil2)
-                    self.scriptdesc.append(desc)
-
-        except OSError:
-            print '[CTPanel] Could not find any /usr/script Folder to populate'
-
-    def schanged(self):
-        mysel = self['list'].getSelectedIndex()
-        if mysel >= 0 and mysel < len(self.scriptdesc):
-            self['statuslab'].setText(self.scriptdesc[mysel])
-
-    def mygo(self):
-        if self['list'].getSelectedIndex() >= 0 and self['list'].getSelectedIndex() < len(self.scriptdesc):
-            mysel = self['list'].getCurrent()
-            mysel2 = self.scriptsPath + mysel + '.sh'
-            mytitle = 'Script: ' + mysel
-            self.session.open(Console, title=mytitle, cmdlist=[mysel2])
+	def Run(self,answer):
+		if answer is True:
+			if not access("/usr/scripts/" + self.sel, X_OK):
+				chmod("/usr/scripts/" + self.sel, 0755)
+			cmd1 = ". /usr/scripts/" + self.sel
+			self.session.open(Console, title=self.sel, cmdlist = [cmd1], closeOnSuccess = False)	
+					
+	def myclose(self):
+		self.close()
+		
